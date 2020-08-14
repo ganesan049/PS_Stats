1edf20da1ad676a54d605d85a77f28035b2cdc4e
Ganesan Ramesh <ganesan.ramesh@ad.infosys.com>
2020-08-14 15:59:20 +0530

    navbar

--- src/data.js
+++ src/data.js
@@ -1,5 +1,14 @@
 export const data = [
-    {name:"gans",teamName:"A",password:"abcdefgh"},
-    {name:"abi",teamName:"C",password:"abcdefgh"},
-    {name:"abi",teamName:"C",password:"abcdefgh"},
+    {name:"Jinu",teamName:"TEAM 1",password:"9840173174"},
+    {name:"Arun",teamName:"TEAM 1",password:"9578464369"},
+    {name:"Tharun",teamName:"TEAM 1",password:"8500592014"},
+    {name:"Anjali",teamName:"TEAM 2",password:"9551104860"},
+    {name:"Subhu",teamName:"TEAM 2",password:"8940790141"},
+    {name:"Krishna",teamName:"TEAM 2",password:"8686769607"},
+    {name:"Dharani",teamName:"TEAM 3",password:"9840173174"},
+    {name:"Deepak",teamName:"TEAM 3",password:"9176189781"},
+    {name:"Deepan",teamName:"TEAM 3",password:"9790333598"},
+    {name:"Keerthana",teamName:"TEAM 4",password:"8883417191"},
+    {name:"Varun",teamName:"TEAM 4",password:"9441779496"},
+    {name:"Bhuvana",teamName:"TEAM 4",password:"9710620567"},
 ]
